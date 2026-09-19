95681d017dbbbf241da4719b9da30b3fff33aa41
Tommy Alhamra <45749487+imotD@users.noreply.github.com>
2026-09-19 14:30:45 +0700

    Revert "Expand CI coverage and add environment template"

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
 	forbidOnly: !!process.env.CI,
 	retries: process.env.CI ? 2 : 0,
 	workers: 1,
-	reporter: process.env.CI ? 'html' : 'list',
+	reporter: 'list',
 	use: {
 		baseURL: 'http://localhost:4173',
 		trace: 'on-first-retry'
